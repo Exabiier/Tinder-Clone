80d84ca69b153ff0b75d9b6bdab4b9a6b94e0488
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-02-20 14:03:01 -0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-purple-400">Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+
